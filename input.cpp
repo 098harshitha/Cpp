2707c78c0854bc4711bbabd128a248c755e45bfa
098harshitha <dnharshitha06@gmail.com>
2026-03-11 23:04:50 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void printNumber(int n) {
+    cout << n;
+}
+
+int main() {
+    int x;
+    cin >> x;          // User input goes here
+    printNumber(x);
+    return 0;
+}
